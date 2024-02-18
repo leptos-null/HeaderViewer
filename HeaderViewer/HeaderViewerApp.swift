8732260c4c336e5f9cbbe2fbfd5697cb2d6756fc
Leptos <leptos.0.null@gmail.com>
2024-02-17 21:46:11 -0800

    Add Xcode project from app template

---
+++ HeaderViewer/HeaderViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderViewerApp.swift
+//  HeaderViewer
+//
+//  Created by Leptos on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HeaderViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
