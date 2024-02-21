17535593eb0ef834008fcb41047291c37b384d22
Leptos <leptos.0.null@gmail.com>
2024-02-20 16:21:34 -0800

    Move ImageClassPicker into its own file

---
+++ HeaderViewer/ImageClassPicker.swift
@@ -0,0 +1,68 @@
+//
+//  ImageClassPicker.swift
+//  HeaderViewer
+//
+//  Created by Leptos on 2/20/24.
+//
+
+import SwiftUI
+import ClassDump
+
+struct ImageClassPicker: View {
+    let namedNode: NamedNode
+    @Binding private var selection: RuntimeObjectType?
+    @State private var searchString: String = ""
+    
+    @EnvironmentObject private var objc: ObjcRuntime
+    
+    private var classNames: [String] {
+        CDUtilities.classNamesIn(image: namedNode.path)
+    }
+    
+    init(namedNode: NamedNode, selection: Binding<RuntimeObjectType?>) {
+        self.namedNode = namedNode
+        _selection = selection
+    }
+    
+    private var runtimeObjects: [RuntimeObjectType] {
+        let ret: [RuntimeObjectType] = classNames.map { .class(named: $0) }
+        if searchString.isEmpty { return ret }
+        return ret.filter { $0.name.localizedCaseInsensitiveContains(searchString) }
+    }
+    
+    var body: some View {
+        Group {
+            if objc.isImageLoaded(path: namedNode.path) {
+                let runtimeObjects = self.runtimeObjects
+                if runtimeObjects.isEmpty {
+                    VStack {
+                        Text("\(namedNode.name) is loaded however does not appear to contain any classes")
+                            .padding(.top)
+                        Spacer()
+                    }
+                    .scenePadding()
+                } else {
+                    List(runtimeObjects, selection: $selection) { runtimeObject in
+                        RuntimeObjectRow(type: runtimeObject)
+                    }
+                    .id(runtimeObjects) // don't try to diff the List
+                    .searchable(text: $searchString)
+                }
+            } else {
+                VStack {
+                    Text("\(namedNode.name) is not yet loaded")
+                        .padding(.top)
+                    Button {
+                        CDUtilities.loadImage(at: namedNode.path)
+                    } label: {
+                        Text("Load now")
+                    }
+                    .buttonStyle(.bordered)
+                    Spacer()
+                }
+                .scenePadding()
+            }
+        }
+        .navigationTitle(namedNode.name)
+    }
+}
