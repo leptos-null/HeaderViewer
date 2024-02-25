2efdcc3d918588e7dac9d063d29bc68e5303def3
Leptos <leptos.0.null@gmail.com>
2024-02-24 17:12:39 -0800

    Implement custom ListView on macOS for lazy loading

--- HeaderViewer/ContentView.swift
+++ HeaderViewer/ContentView.swift
@@ -16,7 +16,7 @@ private enum RuntimeTypeSearchScope: Hashable {
 
 struct ContentView: View {
     @StateObject private var listings: RuntimeListings = .shared
-
+    
     @State private var selectedObject: RuntimeObjectType?
     @State private var searchString: String = ""
     @State private var searchScope: RuntimeTypeSearchScope = .all
@@ -37,7 +37,7 @@ struct ContentView: View {
     var body: some View {
         NavigationSplitView {
             NavigationStack {
-                List(runtimeObjects, selection: $selectedObject) { runtimeObject in
+                ListView(runtimeObjects, selection: $selectedObject) { runtimeObject in
                     RuntimeObjectRow(type: runtimeObject)
                 }
                 .id(runtimeObjects) // don't try to diff the List
@@ -83,3 +83,67 @@ struct ContentView: View {
         }
     }
 }
+
+private struct ListView<DataSource: RandomAccessCollection, SelectionValue: Hashable, RowContent: View>: View
+where DataSource.Element: Identifiable, SelectionValue == DataSource.Element.ID /* List does not have this last constraint */ {
+    @Binding private var selection: SelectionValue?
+    private let dataSource: DataSource
+    private let rowBuilder: (DataSource.Element) -> RowContent
+    
+    init(_ dataSource: DataSource, selection: Binding<SelectionValue?>, @ViewBuilder rowBuilder: @escaping (DataSource.Element) -> RowContent) {
+        _selection = selection
+        self.dataSource = dataSource
+        self.rowBuilder = rowBuilder
+    }
+    
+    var body: some View {
+#if os(macOS)
+        // `List` is not lazy on macOS
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 0) {
+                ForEach(dataSource) { dataItem in
+                    Row(isSelected: .init(get: {
+                        dataItem.id == selection
+                    }, set: { isSelected in
+                        selection = dataItem.id
+                    })) {
+                        rowBuilder(dataItem)
+                    }
+                }
+            }
+            .padding(.horizontal, 10)
+        }
+#else
+        List(dataSource, selection: $selection, rowContent: rowBuilder)
+#endif
+    }
+}
+
+#if os(macOS)
+private extension ListView {
+    struct Row<Content: View>: View {
+        @Binding var isSelected: Bool
+        let content: () -> Content
+        
+        init(isSelected: Binding<Bool>, @ViewBuilder content: @escaping  () -> Content) {
+            _isSelected = isSelected
+            self.content = content
+        }
+        
+        var body: some View {
+            content()
+                .padding(6)
+                .contentShape(.interaction, Rectangle())
+                .background {
+                    if isSelected {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(.tint)
+                    }
+                }
+                .onTapGesture {
+                    isSelected = true
+                }
+        }
+    }
+}
+#endif
