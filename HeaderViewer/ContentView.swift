5f71fb1984909b95b3f66257401333496098d447
Leptos <leptos.0.null@gmail.com>
2024-02-24 21:53:50 -0800

    Split the root view from ContentView into ContentRootView

--- HeaderViewer/ContentView.swift
+++ HeaderViewer/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 import ClassDump
 
+struct ContentView: View {
+    @StateObject private var listings: RuntimeListings = .shared
+    
+    @State private var selectedObject: RuntimeObjectType?
+    
+    var body: some View {
+        NavigationSplitView {
+            ContentRootView(selectedObject: $selectedObject)
+                .environmentObject(listings)
+        } detail: {
+            if let selectedObject {
+                NavigationStack {
+                    RuntimeObjectDetail(type: selectedObject)
+                        .navigationDestination(for: RuntimeObjectType.self) { object in
+                            RuntimeObjectDetail(type: object)
+                        }
+                }
+            } else {
+                Text("Select a class or protocol")
+                    .scenePadding()
+            }
+        }
+        .navigationSplitViewStyle(.balanced)
+    }
+}
+
 private enum RuntimeTypeSearchScope: Hashable {
     case all
     case classes
     case protocols
 }
 
-struct ContentView: View {
-    @StateObject private var listings: RuntimeListings = .shared
+struct ContentRootView: View {
+    @EnvironmentObject private var listings: RuntimeListings
     
-    @State private var selectedObject: RuntimeObjectType?
+    @Binding var selectedObject: RuntimeObjectType?
     @State private var searchString: String = ""
     @State private var searchScope: RuntimeTypeSearchScope = .all
     
@@ -33,55 +59,39 @@ struct ContentView: View {
         return ret.filter { $0.name.localizedCaseInsensitiveContains(searchString) }
     }
     
-    
     var body: some View {
-        NavigationSplitView {
-            NavigationStack {
-                let runtimeObjects = self.runtimeObjects
-                ListView(runtimeObjects, selection: $selectedObject) { runtimeObject in
-                    RuntimeObjectRow(type: runtimeObject)
-                }
-                .id(runtimeObjects) // don't try to diff the List
-                .searchable(text: $searchString)
-                .searchScopes($searchScope) {
-                    Text("All")
-                        .tag(RuntimeTypeSearchScope.all)
-                    Text("Classes")
-                        .tag(RuntimeTypeSearchScope.classes)
-                    Text("Protocols")
-                        .tag(RuntimeTypeSearchScope.protocols)
-                }
-                .navigationTitle("Header Viewer")
-                .toolbar {
-                    ToolbarItem {
-                        NavigationLink(value: CDUtilities.dyldSharedCacheImageRootNode) {
-                            Label("System Images", systemImage: "folder")
-                        }
-                    }
-                }
-                .navigationDestination(for: NamedNode.self) { namedNode in
-                    if namedNode.isLeaf {
-                        ImageClassPicker(namedNode: namedNode, selection: $selectedObject)
-                            .environmentObject(listings)
-                    } else {
-                        NamedNodeView(node: namedNode)
-                            .environmentObject(listings)
+        NavigationStack {
+            let runtimeObjects = self.runtimeObjects
+            ListView(runtimeObjects, selection: $selectedObject) { runtimeObject in
+                RuntimeObjectRow(type: runtimeObject)
+            }
+            .id(runtimeObjects) // don't try to diff the List
+            .searchable(text: $searchString)
+            .searchScopes($searchScope) {
+                Text("All")
+                    .tag(RuntimeTypeSearchScope.all)
+                Text("Classes")
+                    .tag(RuntimeTypeSearchScope.classes)
+                Text("Protocols")
+                    .tag(RuntimeTypeSearchScope.protocols)
+            }
+            .navigationTitle("Header Viewer")
+            .toolbar {
+                ToolbarItem {
+                    NavigationLink(value: CDUtilities.dyldSharedCacheImageRootNode) {
+                        Label("System Images", systemImage: "folder")
                     }
                 }
             }
-        } detail: {
-            NavigationStack {
-                if let selectedObject {
-                    RuntimeObjectDetail(type: selectedObject)
-                        .navigationDestination(for: RuntimeObjectType.self) { object in
-                            RuntimeObjectDetail(type: object)
-                        }
+            .navigationDestination(for: NamedNode.self) { namedNode in
+                if namedNode.isLeaf {
+                    ImageClassPicker(namedNode: namedNode, selection: $selectedObject)
+                        .environmentObject(listings)
                 } else {
-                    Text("Select a class or protocol")
-                        .scenePadding()
+                    NamedNodeView(node: namedNode)
+                        .environmentObject(listings)
                 }
             }
         }
-        .navigationSplitViewStyle(.balanced)
     }
 }
